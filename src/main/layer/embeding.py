c69e19497de9a8897a014f11533436796d1d0a00
dafaw <dafawiratama13@gmail.com>
2021-11-27 01:07:25 +0700

    Add Input Normalization on Embeding Layer

--- src/main/layer/embeding.py
+++ src/main/layer/embeding.py
@@ -9,8 +9,7 @@ class Embedding:
         self.embedding = np.random.randn(self.embedding_size)
 
     def forward(self, x, training):
-        return x * self.embedding
+        return ((x * self.embedding) / self.embedding_size) - .5
 
     def backward(self, dout, learning_rate):
         return dout
-
